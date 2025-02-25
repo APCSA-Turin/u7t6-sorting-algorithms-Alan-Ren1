605bc56681598e9c09aebd94842cd96d8ac983ad
Alan-Ren1 <alanr94@nycstudents.net>
2025-02-25 00:12:11 +0000

    .

--- src/main/java/com/example/project/Selection_Sort/SelectionSort.java
+++ src/main/java/com/example/project/Selection_Sort/SelectionSort.java
@@ -1,4 +1,5 @@
 package com.example.project.Selection_Sort;
+
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.io.IOException;
@@ -6,20 +7,41 @@ import java.io.File;
 
 public class SelectionSort {
 
-    // PART A. implementing selection sort
+    // PART A: Implementing selection sort for an array of integers
     public static int[] selectionSort(int[] elements) {
-       
+        for (int i = 0; i < elements.length - 1; i++) { 
+            int minIndex = i;
+            for (int j = i + 1; j < elements.length; j++) { 
+                if (elements[j] < elements[minIndex]) {
+                    minIndex = j; 
+                }
+            }
+            // Swap the found minimum element with the first element of the unsorted part
+            int temp = elements[i];
+            elements[i] = elements[minIndex];
+            elements[minIndex] = temp;
+        }
         return elements;
     }
 
-
-    // PART B. sorting a 1000-word list
+    // PART B: Implementing selection sort for an ArrayList of words
     public static ArrayList<String> selectionSortWordList(ArrayList<String> words) {
-       
+        for (int i = 0; i < words.size() - 1; i++) { 
+            int minIndex = i;
+            for (int j = i + 1; j < words.size(); j++) { 
+                if (words.get(j).compareTo(words.get(minIndex)) < 0) {
+                    minIndex = j;
+                }
+            }
+            // Swap words
+            String temp = words.get(i);
+            words.set(i, words.get(minIndex));
+            words.set(minIndex, temp);
+        }
         return words;
     }
 
-    //call this method to load 1000 words into list. Use it to test Part B
+    //call this method to load 1000 words into a list (for testing Part B)
     public static ArrayList<String> loadWordsInto(ArrayList<String> wordList) {
         try {
             Scanner input = new Scanner(new File("src/main/java/com/example/project/Selection_Sort/words.txt"));
@@ -27,9 +49,11 @@ public class SelectionSort {
                 String word = input.next();
                 wordList.add(word);
             }
+            input.close();
         } catch (IOException e) {
-            System.out.println(e.getMessage());
+            System.out.println("Error reading file: " + e.getMessage());
         }
         return wordList;
     }
+
 }
